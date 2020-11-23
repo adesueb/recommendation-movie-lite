b667e006793ebd6997b54b7634401ee272df8e44
ade <ade.sueb@gmail.com>
2020-11-23 11:29:45 +0700

    functionality backup features and labels for 50 min_contents_on_user

--- dataprocessing/backup_features_labels.py
+++ dataprocessing/backup_features_labels.py
@@ -5,7 +5,7 @@ from dataprocessing.save_features_labels import save_data
 from datetime import datetime
 import pandas as pd
 
-MIN_CONTENTS_ON_USER = 250
+MIN_CONTENTS_ON_USER = 50
 MAX_DAYS = 7
 MAX_SEQUENCE = 1
 DATA_DIR = "../data"
@@ -13,7 +13,8 @@ DATA_DIR = "../data"
 recommandation_df = pd.read_csv('{}/data.csv'.format(DATA_DIR)).sort_values(by=['time'])
 
 now = datetime.now()
-filePath = '{}/backup_labels-{}.txt'.format(DATA_DIR, now)
+filePathLabels = '{}/backup_labels-{}.txt'.format(DATA_DIR, now)
+filePathFeatures = '{}/backup_features-{}.txt'.format(DATA_DIR, now)
 sortedClassContents = getSortedClassContents(recommandation_df, MIN_CONTENTS_ON_USER)
 saveClassesToFile(sortedClassContents)
 lenSortedClassContents = len(sortedClassContents)
@@ -22,29 +23,29 @@ print(lenSortedClassContents)
 data1 = pd.read_csv('{}/data1.csv'.format(DATA_DIR)).sort_values(by=['time'])
 training_data = build_training_data(data1, sortedClassContents, MAX_SEQUENCE)
 print(len(training_data))
-save_data(training_data, filePath)
+save_data(training_data, filePathFeatures, filePathLabels)
 
 data2 = pd.read_csv('{}/data2.csv'.format(DATA_DIR)).sort_values(by=['time'])
 training_data = np.concatenate(training_data, build_training_data(data2, sortedClassContents, MAX_SEQUENCE))
 print(len(training_data))
-save_data(training_data, filePath)
+save_data(training_data, filePathFeatures, filePathLabels)
 
 data3 = pd.read_csv('{}/data3.csv'.format(DATA_DIR)).sort_values(by=['time'])
 training_data = np.concatenate((training_data, build_training_data(data3, sortedClassContents, MAX_SEQUENCE)))
 print(len(training_data))
-save_data(training_data, filePath)
+save_data(training_data, filePathFeatures, filePathLabels)
 
 data4 = pd.read_csv('{}/data4.csv'.format(DATA_DIR)).sort_values(by=['time'])
 training_data = np.concatenate((training_data, build_training_data(data4, sortedClassContents, MAX_SEQUENCE)))
 print(len(training_data))
-save_data(training_data, filePath)
+save_data(training_data, filePathFeatures, filePathLabels)
 
 data5 = pd.read_csv('{}/data5.csv'.format(DATA_DIR)).sort_values(by=['time'])
 training_data = np.concatenate((training_data, build_training_data(data5, sortedClassContents, MAX_SEQUENCE)))
 print(len(training_data))
-save_data(training_data, filePath)
+save_data(training_data, filePathFeatures, filePathLabels)
 
 data6 = pd.read_csv('{}/data6.csv'.format(DATA_DIR)).sort_values(by=['time'])
 training_data = np.concatenate((training_data, build_training_data(data6, sortedClassContents, MAX_SEQUENCE)))
 print(len(training_data))
-save_data(training_data, filePath)
+save_data(training_data, filePathFeatures, filePathLabels)
--- dataprocessing/save_features_labels.py
+++ dataprocessing/save_features_labels.py
@@ -3,18 +3,18 @@ from datetime import datetime
 import numpy as np
 
 
-def save_data(data, dataPath):
+def save_data(data, featuresPath, labelPath):
     features = []
     labels = []
     for feature, label in data:
         features.append(feature)
         labels.append(label)
 
-    a_file = open(dataPath, "w")
+    a_file = open(featuresPath, "w")
     for row in features:
         np.savetxt(a_file, row[0])
     a_file.close()
 
-    a_file = open(dataPath, "w")
+    a_file = open(labelPath, "w")
     np.savetxt(a_file, label)
     a_file.close()
